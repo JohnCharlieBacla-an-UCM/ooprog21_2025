658ebd047c31e27327b400d92b8c22a2d7578779
JohnCharlieBaclaan-UCM <johncharliebolvax@gmail.com>
2025-09-16 09:07:43 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,16 @@
+// iqwdiqhwdqd
+
+/*
+ iqwhdiqwdhuwdu
+ jqwdowqjdowqd
+*/
+
+/**
+ * qhwdiqwhd
+ * qhdqwdqdqd
+ */
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println("This code contains the 3 types of comments.");
+    }
+}
---
+++ FavoriteSong.java
@@ -0,0 +1,9 @@
+
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("So you can keep me,");
+        System.out.println("inside the pocket of your ripped jeans?,");
+        System.out.println("Holding me closer til' our eyes meet,");
+        System.out.println("you wont ever be alone, wait from me to come home.");
+    }
+}
