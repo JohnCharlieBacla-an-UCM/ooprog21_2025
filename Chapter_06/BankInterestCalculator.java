83a9e685cf9299076b65d1b13b3b551ae3777844
JohnCharlieBaclaan-UCM <johncharliebolvax@gmail.com>
2025-10-21 08:50:00 +0800

    Update BankInterestCalculator.java

--- Chapter_06/BankInterestCalculator.java
+++ Chapter_06/BankInterestCalculator.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class BankInterestCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double balance;
+        final double INTEREST_RATE = 0.03; // 3% fixed interest rate
+        char choice = 'Y';
+        int year = 1;
+
+        System.out.print("Enter your initial balance: ");
+        balance = input.nextDouble();
+
+        
+        while (choice == 'Y' || choice == 'y') {
+            
+            balance = balance + (balance * INTEREST_RATE);
+
+            System.out.printf("Year %d: Balance = %.2f%n", year, balance);
+
+            System.out.print("Do you want to continue to next year? (Y/N): ");
+            choice = input.next().charAt(0);
+
+            year++;
+        }
